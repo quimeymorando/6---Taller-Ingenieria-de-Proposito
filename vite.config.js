526560e90b9929295c9d45bba9ae4f5f04888695
quimeymorando <kimi.morando@gmail.com>
2026-02-19 16:39:05 -0300

    fix: updated base path to './' to fix 404 asset errors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/taller-ingenieria-de-proposito/', // Allows opening dist/index.html directly
+    base: './', // Use relative paths for compatibility with Vercel and local dist
 })
